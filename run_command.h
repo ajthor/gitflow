f09569eb175f2e36a8d3e5171cc564ea3c85c89d
Adam <adam.thorpe.g@gmail.com>
2014-05-09 15:37:02 -0600

    make definitions extern

--- run_command.h
+++ run_command.h
@@ -1,17 +1,14 @@
-#ifndef RUN_COMMAND_H
-#define RUN_COMMAND_H
-
 static char const * builtin_commands[] = {
-	"feature",
-	"patch"
 };
 
 static char const * shell_commands[] = {
+	"feature",
+	"patch",
 	"gh-pages",
 	"init"
 };
 
-int exec_gitflow_command(char const * cmd, int argc, char const * argv[]);
-int exec_shell_command(char const * cmd, int argc, char const * argv[]);
+extern int exec_gitflow_command(char const * cmd, int argc, char const * argv[]);
+extern int exec_shell_command(char const * cmd, int argc, char const * argv[]);
 
-#endif /* RUN_COMMAND_H */
+
