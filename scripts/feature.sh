c34ec741edd45a841127536f73fb9acfc190fc73
Adam <adam.thorpe.g@gmail.com>
2014-05-13 23:59:51 -0600

    add local features
    remote features coming soon

--- scripts/feature.sh
+++ scripts/feature.sh
@@ -2,11 +2,17 @@
 
 USAGE="usage: gitflow feature <branch_name> [-d | --delete] [-m | --merge] [<destination_branch>]\n\n"
 
-delete=
-merge=
+delete=0
+merge=0
+
+remoteUrl=
+has_remote=0
+
+branch_exists=0
+dest_exists=0
 
 branch=
-dest=
+dest="development"
 
 while test $# != 0
 do
@@ -19,7 +25,7 @@ do
 			merge=1
 			;;
 		dev | develop | development | master)
-			dest="$1"
+			dest="${1:-development}"
 			;;
 		-h | --help)
 			printf "${USAGE}"
@@ -32,37 +38,83 @@ do
 			;;
 		*)
 			branch="$1"
-
-			if [ -n "${branch#release*}" ] || [ -n "${branch#issue*}"]; then
-				printf "ERROR: Cannot use branch names that begin with \'release\' or \'issue\'.\n"
-				printf "Use the \'gitflow release\' or \'gitflow patch\' command.\n"
-				exit 1
-			fi
-			# Is branch a branch that currently exists?
+			# if [ -n "${branch#release*}" ] || [ -n "${branch#issue*}"]; then
+			# 	printf "ERROR: Cannot use branch names that begin with \'release\' or \'issue\'.\n"
+			# 	printf "Use the \'gitflow release\' or \'gitflow patch\' command.\n"
+			# 	exit 1
+			# fi
 			;;
 	esac
 	shift
 
 done
 
+# Exit if no branch name specified.
 if [ -z "$branch" ]; then
-	printf "Must supply a <branch_name> to feature command."
+	printf "Must supply a <branch_name> to feature command.\n"
 	exit 1
+fi
+
+# Feature Branch
+# ==============
+
+# Check if Feature Branch Exists
+# ------------------------------
+if git show-ref --verify -q refs/heads/"$branch"; then
+	branch_exists=1
+fi
+
+if [ "$branch_exists" -eq 1 ]; then
+
+	has_remote=$(git ls-remote ${remoteUrl} &> /dev/null)
+	if [ -n "$has_remote" ]; then
+		has_remote=1
+	else
+		has_remote=0
+	fi
+
+	# Merge Branch
+	# ------------
+	# Make sure destination exists and then merge the feature branch 
+	# into it.
+	if [ "$merge" -eq 1 ]; then
+		if git show-ref --verify -q refs/heads/"$dest"; then
+			dest_exists=1
+		else
+			printf "Destination branch must exist.\n"
+			exit 1
+		fi
+
+		printf "Merge: $dest .. $branch\n"
+		
+		# if [ "$has_remote" -eq 1 ]; then
+		# 	git pull origin "$dest"
+		# fi
+		
+		git checkout "$dest" &&
+		git merge --no-ff "$branch"
+
+		# if [ "$has_remote" -eq 1 ]; then
+		# 	git push
+		# fi
+	fi
+
+	# Delete Branch
+	# -------------
+	# If the delete flag is passed, delete the branch. Otherwise, 
+	# check out the branch.
+	if [ "$delete" -eq 1 ]; then
+		printf "Delete: $branch\n"
+		git branch -d "$branch"
+	else
+		git checkout "$branch"
+	fi
+
 else
+	# Create Feature Branch
+	# ---------------------
 	git checkout -b "$branch" development
 fi
 
-if [[ $merge ]]; then
-	printf "Merge: $branch .. development"
-	git pull origin development &&
-	git checkout development &&
-	git merge --no-ff "$branch" &&
-	git push
-fi
-
-if [[ $delete ]]; then
-	printf "Delete: $branch"
-	git branch -d "$branch"
-fi
 
 
