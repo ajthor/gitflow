5789ac3f831cd34882b06086b1a35858490b7b1b
Adam <adam.thorpe.g@gmail.com>
2014-05-14 12:42:20 -0600

    update local release script
    remote functionality will be included in a later release

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,6 +12,7 @@ version=
 branch=
 
 branch_exists=0
+tag_exists=0
 
 while test $# != 0
 do
@@ -41,35 +42,92 @@ do
 
 done
 
+# Exit if no tag supplied.
 if [ -z "$branch" ]; then
 	printf "Must supply a <semver_tag> to release command.\n"
 	exit 1
+fi
+
+# Release Branch
+# ==============
+
+# Check if Release Branch Exists
+# ------------------------------
+if git show-ref --verify -q refs/heads/"$branch"; then
+	branch_exists=1
+fi
+# Check if Tag Exists
+# -------------------
+if git show-ref --verify -q refs/tags/"$version"; then
+	tag_exists=1
+fi
+
+if [ "$branch_exists" -eq 1 ]; then
+
+	git stash
+
+	# Merge Branch
+	# ------------
+	# Merge release branch into master and development, tagging the 
+	# release as you go.
+	if [ "$merge" -eq 1 ]; then
+
+		has_remote=$(git ls-remote ${remoteUrl} &> /dev/null)
+		if [ -n "$has_remote" ]; then
+			has_remote=1
+		else
+			has_remote=0
+		fi
+
+		printf "Merge: master .. $branch\n"
+
+		# if [ "$has_remote" -eq 1 ]; then
+		# 	git pull origin master
+		# 	git pull origin development
+		# fi
+
+		git checkout master &&
+		git merge --no-ff "$branch" 
+
+		printf "Tag: $version"
+		git tag -a "$version"
+		
+		# if [ "$has_remote" -eq 1 ]; then
+		# 	git push
+		# 	git push --tags
+		# fi
+
+		printf "Merge: development .. $branch\n"
+		
+		git checkout development &&
+		git merge --no-ff "$branch"
+
+		# if [ "$has_remote" -eq 1 ]; then
+		# 	git push
+		# fi
+	fi
+
+	# Delete Branch
+	# -------------
+	# If the delete flag is passed, delete the branch. Otherwise, 
+	# check out the branch.
+	if [[ $delete ]]; then
+		printf "Delete: $branch\n"
+		git branch -d "$branch"
+	else
+		git checkout "$branch"
+	fi
+
 else
-	git checkout -b "$branch" development
-fi
-
-if [[ $merge ]]; then
-	#statements
-	printf "Merge: master .. $branch\n"
-	git pull origin master &&
-	git checkout master &&
-	git merge --no-ff "$branch" &&
-	git push
-
-	printf "Tag: $version"
-	git tag -a "$version" &&
-	git push --tags
-
-	printf "Merge: development .. $branch\n"
-	git pull origin development &&
-	git checkout development &&
-	git merge --no-ff "$branch" &&
-	git push 
-fi
-
-if [[ $delete ]]; then
-	printf "Delete: $branch\n"
-	git branch -d "$branch"
+	# Create release Branch
+	# ---------------------
+	# If tag does not exist, create release branch.
+	if [ "$tag_exists" -eq 1 ]; then
+		printf "Error: tag already exists. Please choose another tag.\n"
+	else
+		printf "Create: release branch $branch\n"
+		git checkout -b "$branch" development
+	fi
 fi
 
 
