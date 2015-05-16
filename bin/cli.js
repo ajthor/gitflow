a06bd956245627ce9d67b04cbba39d95df913797
Adam <adam.thorpe.g@gmail.com>
2015-05-16 16:01:13 -0600

    add cli functionality to parse arguments

--- bin/cli.js
+++ bin/cli.js
@@ -2,4 +2,54 @@
 
 'use strict';
 
-console.log('Hello, World!');
+// Command-Line Tool
+// =================
+// This is a typical Node.js CLI program.
+
+var usageMsg = 'usage:  gitflow <command> [options]';
+
+var usage = function () {
+	console.log(usageMsg);
+};
+
+// Parse Arguments
+// ---------------
+// Parses all arguments passed to the program.
+
+var parseArgv = function (argv) {
+	var i, arg;
+	var obj = {
+		command: '',
+		flags: [],
+		options: []
+	};
+
+	for (i = argv.length; i--; ) {
+		arg = argv[i];
+
+		if (arg.slice(2) === '--') { // Option
+			obj[ arg.slice(2) ] = true;
+		}
+		else if (arg[0] === '-') { // Flag
+			obj.flags.push(arg);
+		}
+		else { // Command
+			obj.command = arg;
+		}
+	}
+
+	return obj;
+};
+
+// CLI
+// ---
+// The command-line tool begins here.
+
+console.log('GitFlow CLI');
+
+var args = process.argv.slice(2);
+var obj = parseArgv(args);
+
+
+
+
